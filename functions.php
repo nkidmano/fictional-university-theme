4eebd15f915a9f74d9418ad8ddd69a9338d53052
nkidmano <nobudy55@gmail.com>
2018-11-02 00:38:21 +0700

    update page

--- functions.php
+++ functions.php
@@ -7,6 +7,10 @@
     wp_enqueue_style('university-main-styles', get_stylesheet_uri(), NULL, microtime());
   }
 
-  add_action('wp_enqueue_scripts', 'university_files');
+  function university_features() {
+    add_theme_support('title-tag');
+  }
 
+  add_action('wp_enqueue_scripts', 'university_files');
+  add_action('after_setup_theme', 'university_features');
 ?>
